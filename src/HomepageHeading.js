0a57beb4da3a7b3da326741bb5f0b400dec11bd1
thierry-lav <lav.thierry@gmail.com>
2019-08-27 13:45:51 +0200

    split into component

---
+++ src/HomepageHeading.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as="h1"
+      content="Imagine-a-Company"
+      inverted
+      style={{
+        fontSize: mobile ? "2em" : "4em",
+        fontWeight: "normal",
+        marginBottom: 0,
+        marginTop: mobile ? "1.5em" : "3em"
+      }}
+    />
+    <Header
+      as="h2"
+      content="Do whatever you want when you want to."
+      inverted
+      style={{
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em"
+      }}
+    />
+  </Container>
+);
+
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool
+};
+
+export default HomepageHeading;
