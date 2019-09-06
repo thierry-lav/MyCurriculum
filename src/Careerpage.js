ec1ff7a2dc19d4511bb15bb6c4e0007fbee4ddda
thierry-lav <lav.thierry@gmail.com>
2019-09-06 13:39:49 +0200

    loop on data

--- src/Careerpage.js
+++ src/Careerpage.js
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import ResponsiveContainer from "./ResponsiveContainer";
 import Footpage from "./Footpage";
-import {
-  Segment,
-  Container,
-  Header,
-  Icon,
-  Card,
-  Image,
-  Reveal
-} from "semantic-ui-react";
+import { Segment, Container, Header, Icon, Card } from "semantic-ui-react";
 import TopMenu from "./TopMenu";
-import TransitionBody from "./TransitionBody";
+
+const parcours = {
+  list: [
+    {
+      id: "1",
+      titre: "Mon parcours professionnel",
+      icon: "rocket",
+      blocks: [
+        {
+          id: "1",
+          entreprise: "Crédit Agricole",
+          date: "Mars 2013 - Mars 2014",
+          description: "Building content ..."
+        },
+        {
+          id: "2",
+          entreprise: "Société Générale",
+          date: "Mars 2013 - Mars 2014",
+          description: "Building content ..."
+        },
+        {
+          id: "3",
+          entreprise: "BNP Paribas",
+          date: "Mars 2012 - Mars 2013",
+          description: "Building content ..."
+        }
+      ]
+    },
+    {
+      id: "2",
+      titre: "Mon parcours académique",
+      icon: "graduation cap",
+      blocks: [
+        {
+          id: "1",
+          entreprise: "Université Paris-Est Créteil",
+          date: "Joined in 2009",
+          description: "Building content ..."
+        },
+        {
+          id: "2",
+          entreprise: "Lycée Turgot",
+          date: "Joined in 2009",
+          description: "Building content ..."
+        },
+        {
+          id: "3",
+          entreprise: "Lycée Jean-Lurçat",
+          date: "Joined in 2005",
+          description: "Building content ..."
+        }
+      ]
+    }
+  ]
+};
 
 function CareerLayout() {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <ResponsiveContainer>
       <Segment
@@ -24,131 +72,40 @@ function CareerLayout() {
       >
         <TopMenu />
       </Segment>
-      <Segment vertical basic>
-        <Header as="h2" icon textAlign="center">
-          <Icon name="rocket" circular />
-          Mon parcours professionnel
-        </Header>
-        <Container>
-          <Card.Group>
-            <Card>
-              <Card.Content>
-                <Card.Header>Crédit Agricole</Card.Header>
-                <Card.Meta>
-                  <span className="date">Mars 2013 - Mars 2014</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Card.Header>Société Générale (Madrid)</Card.Header>
-                <Card.Meta>
-                  <span className="date">Mars 2013 - Mars 2014</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Card.Header>Société Générale</Card.Header>
-                <Card.Meta>
-                  <span className="date">Mars 2013 - Mars 2014</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Card.Header>BNP Paribas</Card.Header>
-                <Card.Meta>
-                  <span className="date">Novembre 2012 - Mars 2013</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </Container>
-      </Segment>
-      <Segment vertical basic>
-        <Header as="h2" icon textAlign="center">
-          <Icon name="graduation cap" circular />
-          Mon parcours scolaire
-        </Header>
-        <Container>
-          <Card.Group>
-            <Card>
-              <Card.Content>
-                <Card.Header>Université Paris-Est Créteil</Card.Header>
-                <Card.Meta>
-                  <span className="date">Joined in 2009</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Card.Header>Lycée Turgot</Card.Header>
-                <Card.Meta>
-                  <span className="date">Joined in 2007</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-            <Card>
-              <Card.Content>
-                <Card.Header>Lycée Jean-Lurçat</Card.Header>
-                <Card.Meta>
-                  <span className="date">Joined in 2005</span>
-                </Card.Meta>
-                <Card.Description>Building content ...</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="eye" />
-                  Voir plus
-                </a>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </Container>
-      </Segment>
+
+      {parcours.list.map(item => {
+        return (
+          <Segment vertical basic>
+            <Header as="h2" icon textAlign="center">
+              <Icon name={item.icon} circular />
+              {item.titre}
+            </Header>
+            <Container>
+              <Card.Group>
+                {item.blocks.map(i => {
+                  return (
+                    <Card>
+                      <Card.Content>
+                        <Card.Header>{i.entreprise}</Card.Header>
+                        <Card.Meta>
+                          <span className="date">{i.date}</span>
+                        </Card.Meta>
+                        <Card.Description>{i.description}</Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a>
+                          <Icon name="eye" />
+                          Voir plus
+                        </a>
+                      </Card.Content>
+                    </Card>
+                  );
+                })}
+              </Card.Group>
+            </Container>
+          </Segment>
+        );
+      })}
       <Footpage />
     </ResponsiveContainer>
   );
